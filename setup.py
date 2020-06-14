11ebdd395a43feb727bcd1d801a10f66c8822b8e
Dennis Staiger <d.staiger@gmail.com>
2020-06-14 09:27:08 +0000

    Required libraries added to PiPI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="swissweather",
-    version="0.1.0",
+    version="0.2.0",
     author="Dennis Staiger",
     author_email="dnsstaiger@gmx.net",
     description="SRF Meteo weather forecast in Switzerland",
@@ -18,5 +18,11 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+          'requests',
+          'oauthlib',
+          'requests_oauthlib',
+          'aiohttp',
+      ],
     python_requires='>=3.8',
 )
