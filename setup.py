698952a6c07fbbb0629ad8b865936405219e6ee7
Dennis Staiger <d.staiger@gmail.com>
2020-06-14 10:14:36 +0000

    Excluding test packages from PyPI

--- setup.py
+++ setup.py
@@ -5,25 +5,26 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="swissweather",
-    version="0.2.2",
+    version="0.2.3",
     author="Dennis Staiger",
     author_email="dnsstaiger@gmx.net",
     description="SRF Meteo weather forecast in Switzerland",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/davosian/swissweather",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["tests"]),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Home Automation',
+        'Topic :: Software Development :: Libraries :: Python Modules'
     ],
     install_requires=[
           'requests',
           'oauthlib',
           'requests_oauthlib',
-          'aiohttp',
-          'python-dotenv'
+          'aiohttp'
       ],
     python_requires='>=3.8',
 )
